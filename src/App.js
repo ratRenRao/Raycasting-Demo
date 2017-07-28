e372a6eba44e2c3d886877403075214342b8002c
Brandon Richardson <brandon.richardson@extendhealth.com>
2017-07-28 16:17:44 -0700

    Update repo. Work in progress

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import GameController from './Controllers/GameController';
+import Game from './Controllers/GameController';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <GameController/>
+        <Game/>
       </div>
     );
   }
--- src/Controllers/GameController.js
+++ src/Controllers/GameController.js
@@ -5,64 +5,56 @@ import React, { Component } from 'react'
 //import RaycastGraphics from "./GraphicsController";
 //import {Controls} from "GraphicsController"
 //import Player from 'PlayerController'
+import SkyImage from '../assets/deathvalley_panorama.jpg'
+import WallImage from '../assets/wall_texture.jpg'
+import './GameController.css'
 
 const CIRCLE = Math.PI * 2;
 
 class GameController extends Component {
     constructor() {
         super();
-        let player = new Player(15.3, -1.2, Math.PI * 0.3);
         let map = new Map(24);
-        let controls = new Controls();
-        //let canvas = React.createElement('canvas', {width: 400, height: 500});
-        let loop = new GameLoop();
-        map.randomize();
+
+        this.player = new Player(15.3, -1.2, Math.PI * 0.3);
+        this.controls = new Controls();
+        this.loop = new GameLoop();
+        //map.randomize();
+        this.map = map;
+        this.resolution = 700;
 
         this.state = {
-            codes: {37: 'left', 39: 'right', 38: 'forward', 40: 'backward'},
-            states: {'left': false, 'right': false, 'forward': false, 'backward': false},
-            resolution: 320,
-            focalLength: 0.8,
-            left: false,
-            right: false,
-            forward: false,
-            backward: false,
-            player: player,
-            map: map,
-            loop: loop,
-            controls: controls,
-            context: null,
+            // resolution: 1080,
+            // focalLength: 0.8,
+            // left: false,
+            // right: false,
+            // forward: false,
+            // backward: false,
+            // player: player,
+            // map: map,
+            // loop: loop,
+            // controls: controls,
+            // context: null,
         };
     }
 
     render() {
-        //return <RaycastGraphics controls={this.state.controls} resolution={this.state.resolution} focalLength={this.state.focalLength} />
         return (
-            <div>
-                <div onKeyDown={(e) => this.state.controls.handleKeyPressed(e)} onKeyUp={(e) => this.state.controls.handleKeyReleased(e)} tabIndex="0">
-                    <button type="button" onClick={() => this.start()} >Start</button>
-                    <canvas width="400" height="500" ref={(x) => this.canvas = x} />
-                </div>
+            <div id="mainDiv" onKeyDown={(e) => this.controls.handleKeyPressed(e)} onKeyUp={(e) => this.controls.handleKeyReleased(e)} tabIndex="0">
+                <button type="button" onClick={() => this.start(this.controls, this.map, this.player)} >Start</button>
+                <canvas width={this.resolution} height={this.resolution} ref={(x) => this.canvas = x} />
             </div>
         );
     }
 
-    start() {
-        let camera = new Camera(this.canvas, 320, 0.8);
+    start(controls, map, player) {
+        let camera = new Camera(this.canvas, this.resolution, 0.8);
 
-        this.state.loop.start(function frame(seconds) {
-            this.state.map.update(seconds);
-            this.state.player.update(this.state.controls.states, this.state.map, seconds);
-            camera.render(this.state.player, this.state.map);
+        this.loop.start(function frame(seconds) {
+            map.update(seconds);
+            player.update(controls, map, seconds);
+            camera.draw(player, map);
         });
-    }
-}
-
-class StartButton extends Component {
-    render() {
-        return (
-            <button type="button" onClick={this.props.onStartClick}>Start</button>
-        );
     }
 }
 
@@ -83,9 +75,9 @@ class Player{
     walk(distance, map) {
         let dx = Math.cos(this.direction) * distance;
         let dy = Math.sin(this.direction) * distance;
-        if (map.get(this.x + dx, this.y) <= 0) this.x = this.state.x + dx;
-        if (map.get(this.x, this.y + dy) <= 0) this.y = this.state.y + dy;
-        this.paces = this.state.paces + distance;
+        if (map.get(this.x + dx, this.y) <= 0) this.x = this.x + dx;
+        if (map.get(this.x, this.y + dy) <= 0) this.y = this.y + dy;
+        this.paces = this.paces + distance;
     }
 
     update(controls, map, seconds) {
@@ -94,40 +86,42 @@ class Player{
         if (controls.forward) this.walk(3 * seconds, map);
         if (controls.backward) this.walk(-3 * seconds, map);
     }
-};
+}
 
 class Controls{
     constructor() {
-        this.codes = {97: 'left', 100: 'right', 119: 'forward', 115: 'backward'};
-        this.states = {'left': false, 'right': false, 'forward': false, 'backward': false};
+        this.left = false;
+        this.right = false;
+        this.forward = false;
+        this.backward = false;
     }
 
     handleKeyPressed(e) {
-        if (e.charCode === 97)
+        if (e.key === 'a')
         //left
             this.left = true;
-        if (e.charCode === 100)
+        if (e.key === 'd')
         //right
             this.right = true;
-        if (e.charCode === 119)
+        if (e.key === 'w')
         //forward
             this.forward = true;
-        if (e.charCode === 115)
+        if (e.key === 's')
         //backward
             this.backward = true;
     }
 
     handleKeyReleased(e) {
-        if (e.charCode === 97)
+        if (e.key === 'a')
         //left
             this.left = false;
-        if (e.charCode === 100)
+        if (e.key === 'd')
         //right
             this.right = false;
-        if (e.charCode === 119)
+        if (e.key === 'w')
         //forward
             this.forward = false;
-        if (e.charCode === 115)
+        if (e.key === 's')
         //backward
             this.backward = false;
 
@@ -169,35 +163,35 @@ class Bitmap{
 class Map{
     constructor(size) {
         this.size = size;
-        this.wallGrid = //new UInt8Array(size * size);
+        this.wallGrid = //new Uint8Array(size * size);
             [
                     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
                     [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
                     [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
                     [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-                    [1,0,0,0,0,0,2,2,2,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1],
-                    [1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1],
-                    [1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,3,0,0,0,3,0,0,0,1],
-                    [1,0,0,0,0,0,2,0,0,0,2,0,0,0,0,0,0,0,0,0,0,0,0,1],
-                    [1,0,0,0,0,0,2,2,0,2,2,0,0,0,0,3,0,3,0,3,0,0,0,1],
+                    [1,0,0,0,0,0,1,1,1,1,1,0,0,0,0,1,0,1,0,1,0,0,0,1],
+                    [1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1],
+                    [1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,1,0,0,0,1,0,0,0,1],
+                    [1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1],
+                    [1,0,0,0,0,0,1,1,0,1,1,0,0,0,0,1,0,1,0,1,0,0,0,1],
                     [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
                     [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
                     [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+                    [1,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1],
+                    [1,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,1],
                     [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-                    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-                    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-                    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-                    [1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-                    [1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-                    [1,4,0,0,0,0,5,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-                    [1,4,0,4,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-                    [1,4,0,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-                    [1,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
-                    [1,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+                    [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,0,0,1],
+                    [1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1],
+                    [1,1,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1],
+                    [1,1,0,0,0,0,5,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1],
+                    [1,1,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,1,0,0,1],
+                    [1,1,0,1,1,1,1,1,1,0,0,0,0,0,0,0,0,1,1,1,1,0,0,1],
+                    [1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+                    [1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
                     [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
                 ];
-        this.skybox = new Bitmap('assets/deathvalley_panorama.jpg', 2000, 750);
-        this.wallTexture = new Bitmap('assets/wall_texture.jpg', 1024, 1024);
+        this.skybox = new Bitmap(SkyImage, 2000, 750);
+        this.wallTexture = new Bitmap(WallImage, 1024, 1024);
         this.light = 0;
     }
 
@@ -214,15 +208,15 @@ class Map{
         }
     }
 
-    cast(point, angle, range) {
+    cast(player, angle, range) {
         let self = this;
         let sin = Math.sin(angle);
         let cos = Math.cos(angle);
         let noWall = { length2: Infinity };
-        return ray({ x: point.x, y: point.y, height: 0, distance: 0 });
+        return ray({ x: player.x, y: player.y, height: 0, distance: 0 });
 
         function ray(origin) {
-            let stepX = step(sin, cos, origin.x, origin.y);
+            let stepX = step(sin, cos, origin.x, origin.y, false);
             let stepY = step(cos, sin, origin.y, origin.x, true);
             let nextStep = stepX.length2 < stepY.length2
                 ? inspect(stepX, 1, 0, origin.distance, stepX.y)
@@ -238,7 +232,7 @@ class Map{
             return {
                 x: inverted ? y + dy : x + dx,
                 y: inverted ? x + dx : y + dy,
-                length2: dx * dx + dy * dy
+                length2: (dx * dx) + (dy * dy)
             };
         }
 
@@ -269,11 +263,11 @@ class Camera {
         this.spacing = this.width / resolution;
         this.focalLength = focalLength || 0.8;
         this.range = 14;
-        this.lightRange = 5;
+        this.lightRange = 50;
         this.scale = (this.width + this.height) / 1200;
     }
 
-    render(player, map) {
+    draw(player, map) {
         this.drawSky(player.direction, map.skybox, map.light);
         this.drawColumns(player, map);
         //this.drawWeapon(player.weapon, player.paces);
@@ -298,20 +292,16 @@ class Camera {
     drawColumns(player, map) {
         this.ctx.save();
         for (let column = 0; column < this.resolution; column++) {
-            let x = column / this.resolution - 0.5;
-            let angle = Math.atan2(x, this.focalLength);
+            let angle = this.findRayAngle(column);
             let ray = map.cast(player, player.direction + angle, this.range);
             this.drawColumn(column, ray, angle, map);
         }
         this.ctx.restore();
     }
 
-    drawWeapon(weapon, paces) {
-        let bobX = Math.cos(paces * 2) * this.scale * 6;
-        let bobY = Math.sin(paces * 4) * this.scale * 6;
-        let left = this.width * 0.66 + bobX;
-        let top = this.height * 0.6 + bobY;
-        this.ctx.drawImage(weapon.image, left, top, weapon.width * this.scale, weapon.height * this.scale);
+    findRayAngle(column) {
+        let x = column / this.resolution - 0.5;
+        return Math.atan2(x, this.focalLength);
     }
 
     drawColumn(column, ray, angle, map) {
@@ -323,7 +313,7 @@ class Camera {
         while (++hit < ray.length && ray[hit].height <= 0) {
             for (let s = ray.length - 1; s >= 0; s--) {
                 let step = ray[s];
-                let rainDrops = Math.pow(Math.random(), 3) * s;
+                let rainDrops = Math.pow(Math.random(), 2) * s;
                 let rain = (rainDrops > 0) && this.project(0.1, angle, step.distance);
                 if (s === hit) {
                     let textureX = Math.floor(texture.width * step.offset);
@@ -338,9 +328,17 @@ class Camera {
 
                 ctx.fillStyle = '#ffffff';
                 ctx.globalAlpha = 0.15;
-                while (--rainDrops > 0) ctx.fillRect(left, Math.random() * rain.top, 1, rain.height);
+                //while (--rainDrops > 0) ctx.fillRect(left, Math.random() * rain.top, 1, 4/*rain.height*/);
             }
         }
+    }
+
+    drawWeapon(weapon, paces) {
+        let bobX = Math.cos(paces * 2) * this.scale * 6;
+        let bobY = Math.sin(paces * 4) * this.scale * 6;
+        let left = this.width * 0.66 + bobX;
+        let top = this.height * 0.6 + bobY;
+        this.ctx.drawImage(weapon.image, left, top, weapon.width * this.scale, weapon.height * this.scale);
     }
 
     project(height, angle, distance) {
